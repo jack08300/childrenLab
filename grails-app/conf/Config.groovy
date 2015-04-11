8e8c634319ab64c05f1f8f4587ecc6ffa36e5071
jay <jack08300@hotmail.com>
2015-04-11 15:44:10 +0800

    fix permission

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -150,6 +150,7 @@ grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	'/**/js/**':                      ['permitAll'],
 	'/**/css/**':                     ['permitAll'],
 	'/**/images/**':                  ['permitAll'],
-	'/**/favicon.ico':                ['permitAll']
+	'/**/favicon.ico':                ['permitAll'],
+    '/connect/**':                    ['permitAll']
 ]
 
--- grails-app/controllers/childrenlab/ConnectController.groovy
+++ grails-app/controllers/childrenlab/ConnectController.groovy
@@ -1,13 +1,16 @@
 package childrenlab
 
 import grails.converters.JSON
+import grails.plugin.springsecurity.annotation.Secured
 
+@Secured(['IS_AUTHENTICATED_ANONYMOUSLY'])
 class ConnectController {
 
     def device(String model, String cordova, String platform, String uuid, String version){
         if(!uuid){
             log.error("UUID is missing. $model, $cordova, $platform, $uuid, $version")
             render([success: false, message: "uuid is missing"] as JSON)
+            return
         }
 
         def connect = Connect.findByUuid(uuid) ?: new Connect(uuid: uuid)
