11c0772da8cbc65ac2bcc17fba94f3c1032f7cc7
jay <jack08300@hotmail.com>
2015-04-11 15:54:35 +0800

    add user to connect

--- grails-app/controllers/childrenlab/ConnectController.groovy
+++ grails-app/controllers/childrenlab/ConnectController.groovy
@@ -6,6 +6,8 @@ import grails.plugin.springsecurity.annotation.Secured
 @Secured(['IS_AUTHENTICATED_ANONYMOUSLY'])
 class ConnectController {
 
+    def springSecurityService
+
     def device(String model, String cordova, String platform, String uuid, String version){
         if(!uuid){
             log.error("UUID is missing. $model, $cordova, $platform, $uuid, $version")
@@ -15,10 +17,13 @@ class ConnectController {
 
         def connect = Connect.findByUuid(uuid) ?: new Connect(uuid: uuid)
 
+        User user = springSecurityService.getCurrentUser() as User
+
         connect.deviceModel = model
         connect.cordova = cordova
         connect.devicePlatform = platform
         connect.deviceVersion = version
+        connect.user = user
         connect.save(failOnError: true)
 
         render([success: true] as JSON)
